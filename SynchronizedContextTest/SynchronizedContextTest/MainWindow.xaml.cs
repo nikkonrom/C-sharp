80e5e746b28da7b3620953a548af62bb5f5ca6aa
Никита Романюк <nikkonrom@gmail.com>
2018-04-02 10:51:25 +0300

    Added simple WPF project as example of marshalling UI thread from another win SyncronizedContext

---
+++ SynchronizedContextTest/SynchronizedContextTest/MainWindow.xaml.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SynchronizedContextTest
+{
+    public partial class MainWindow
+    {
+        private CancellationTokenSource _cancellationTokenSource;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        public static async Task<string> DownloadAsync(WebClient client, Uri uri, CancellationToken token)
+        {
+            var taskCompletionSource = new TaskCompletionSource<string>();
+
+            client.DownloadStringCompleted += SetResultAction;
+
+            void SetResultAction(object sender, DownloadStringCompletedEventArgs data)
+            {
+                if (data.Cancelled)
+                {
+                    taskCompletionSource.SetCanceled();
+                }
+                else if (data.Error != null)
+                {
+                    taskCompletionSource.SetException(data.Error);
+                }
+                else
+                {
+                    taskCompletionSource.SetResult(data.Result);
+                }
+                client.DownloadStringCompleted -= SetResultAction;
+
+
+            }
+            using (token.Register(client.CancelAsync))
+            {
+                client.DownloadStringAsync(uri);
+
+                return await taskCompletionSource.Task;
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var context = SynchronizationContext.Current;
+
+            var thread = new Thread(DownloadAndTrackAsync);
+            thread.Start(context);
+
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            _cancellationTokenSource.Cancel();
+        }
+
+        private async void DownloadAndTrackAsync(object state)
+        {
+            var context = state as SynchronizationContext;
+            var client = new WebClient();
+            _cancellationTokenSource = new CancellationTokenSource();
+
+            client.DownloadProgressChanged += (sender, args) =>
+            {
+                var progress = Convert.ToDouble(args.BytesReceived) / Convert.ToDouble(args.TotalBytesToReceive) * 100;
+                context?.Post(UpdateProgress, progress);
+            };
+
+            var task = DownloadAsync(client, new Uri("http://ftp.byfly.by/test/10mb.txt"), _cancellationTokenSource.Token);
+            context?.Post(UpdateStatus, "Downloading");
+
+            try
+            {
+                Debug.WriteLine(await task);
+                context?.Post(UpdateStatus, "Succeeded");
+            }
+            catch (TaskCanceledException exception)
+            {
+                Console.WriteLine(exception.Message);
+                context?.Post(UpdateStatus, "Canceled");
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+                context?.Post(UpdateStatus, "Faulted");
+                throw;
+            }
+        }
+
+        private void UpdateProgress(object state)
+        {
+            ProgressBar.Value = (double)state;
+        }
+
+        private void UpdateStatus(object state)
+        {
+            StatusLabel.Content = (string)state;
+        }
+    }
+}
